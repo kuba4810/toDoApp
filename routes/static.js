abec6d31bc1cc217863e9d31d6ac9067544b2bc0
unknown <kuba__koziol@op.pl>
2019-06-10 18:51:22 +0200

    fdgdfg

--- routes/static.js
+++ routes/static.js
@@ -3,7 +3,7 @@ module.exports = (app) => {
 
     // Logowanie 
     app.get('/cos', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../' ,'client', 'todo', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname ,'client', 'todo', 'build', 'index.html'));
     });
 
     // app.get('/cos', function(req, res) {
