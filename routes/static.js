343ac296f08d62a5d611c876dd60ecb22b6cd5ef
unknown <kuba__koziol@op.pl>
2019-06-10 18:55:21 +0200

    asdasd

--- routes/static.js
+++ routes/static.js
@@ -2,12 +2,8 @@ const path = require('path');
 module.exports = (app) => {
 
     // Logowanie 
-    app.get('/cos', (req, res) => {
-        res.sendFile(path.resolve(__dirname , './' ,'client', 'todo', 'build', 'index.html'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname , '../' ,'client', 'todo', 'build', 'index.html'));
     });
-
-    // app.get('/cos', function(req, res) {
-    //     res.send('Hejo :D')
-    //   });
 
 }
