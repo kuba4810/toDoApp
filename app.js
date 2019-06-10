8e9bd141476e15d1e666b6ceff1060b333c6c173
unknown <kuba__koziol@op.pl>
2019-06-10 19:09:05 +0200

    sdfds

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var app = express();
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.static(__dirname + '/client/todo/build'));
+app.use(express.static(__dirname + '/build'));
+
+// app.use(express.static(path.resolve(__dirname, 'build' ,'index.html')));
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/static.js
+++ routes/static.js
@@ -3,7 +3,7 @@ module.exports = (app) => {
 
     // Logowanie 
     app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname , '../' ,'client', 'todo', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname , '../' , 'build', 'index.html'));
     });
 
 }
